467323fb423a47f518639aa35a97c445eba43608
Aditya-190304 <aditya.nigam@kickdrumtech.com>
2026-01-14 14:49:51 +0530

    Update HospitalStaffingApplication.java

--- backend/homework/6/HospitalStaffing/src/main/java/com/hospital/staffing/HospitalStaffingApplication.java
+++ backend/homework/6/HospitalStaffing/src/main/java/com/hospital/staffing/HospitalStaffingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing; // Import added
 
 @SpringBootApplication
-@EnableJpaAuditing // <--- FIX: Enables automatic timestamps
+@EnableJpaAuditing 
 public class HospitalStaffingApplication {
     public static void main(String[] args) {
         SpringApplication.run(HospitalStaffingApplication.class, args);
